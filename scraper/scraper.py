f217266194c8569e28311879c0871bf68ada815b
lanuk <lanuk@users.noreply.github.com>
2024-04-13 17:03:08 -0700

    reorganizing

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,20 +2,21 @@ from db_tools import connect_to_db, create_table, load_data
 # from dotenv import load_dotenv
 from sqlalchemy import Column, Float, MetaData, Table, Text, TIMESTAMP
 import os
+import psycopg2
 import requests
 import time
 
 locations = [
     {"lat":25.8600,"lon":-97.4200},
-    {"lat":25.9000,"lon":-97.5200},
-    {"lat":25.9000,"lon":-97.4800},
-    {"lat":25.9000,"lon":-97.4400},
-    {"lat":25.9000,"lon":-97.4000},
-    {"lat":25.9200,"lon":-97.3800},
-    {"lat":25.9400,"lon":-97.5400},
-    {"lat":25.9400,"lon":-97.5200},
-    {"lat":25.9400,"lon":-97.4800},
-    {"lat":25.9400,"lon":-97.4400}
+    # {"lat":25.9000,"lon":-97.5200},
+    # {"lat":25.9000,"lon":-97.4800},
+    # {"lat":25.9000,"lon":-97.4400},
+    # {"lat":25.9000,"lon":-97.4000},
+    # {"lat":25.9200,"lon":-97.3800},
+    # {"lat":25.9400,"lon":-97.5400},
+    # {"lat":25.9400,"lon":-97.5200},
+    # {"lat":25.9400,"lon":-97.4800},
+    # {"lat":25.9400,"lon":-97.4400}
 ]
 
 meta = MetaData()
@@ -35,7 +36,7 @@ TOMORROW_IO_API_KEY = os.environ['API_KEY']
 
 metric_types = {
     "historical": "https://api.tomorrow.io/v4/weather/history/recent?apikey=",
-    "forecast": "https://api.tomorrow.io/v4/weather/forecast?apikey="
+    # "forecast": "https://api.tomorrow.io/v4/weather/forecast?apikey="
 }
 
 params = {
@@ -87,7 +88,6 @@ def get_all_data():
 
 if __name__ == "__main__":
     output = get_all_data()
-    print(TOMORROW_IO_API_KEY)
     
     # Connect to database
     engine = connect_to_db()
